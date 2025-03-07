f9ee004d2c05f108328c456d2b13322e84c9f0af
Khairul Basar <khairulbashar24853@gmail.com>
2025-03-07 19:56:36 +0600

    initial commit add spring boot application

---
+++ src/test/java/com/qubex/learning_management_system/LearningManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qubex.learning_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
