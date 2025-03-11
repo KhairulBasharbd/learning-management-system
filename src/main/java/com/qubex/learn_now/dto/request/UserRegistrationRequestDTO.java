d96c82b652d4683c69180437d92ba737d436539d
Khairul Basar <khairulbashar24853@gmail.com>
2025-03-12 02:11:28 +0600

    Create UserRegistrationRequestDTO for handling and validating user registraiton

---
+++ src/main/java/com/qubex/learn_now/dto/request/UserRegistrationRequestDTO.java
@@ -0,0 +1,34 @@
+package com.qubex.learn_now.dto.request;
+
+import com.qubex.learn_now.enums.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserRegistrationRequestDTO {
+
+    @NotEmpty(message = "Name can not be empty")
+    private String fullName;
+
+    @NotEmpty(message = "Email can not be empty")
+    @Email(message = "Invalid Email format")
+    private String email;
+
+    @NotEmpty(message = "Password can not be empty")
+    @Size(min = 6, message = "Password must be at least 6 characters long" )
+//    @Pattern(
+//            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$",
+//            message = "Password must contain at least one digit, lowercase, uppercase and special character"
+//    )
+    private String password;
+
+    @NotEmpty(message = "Role can not be empty")
+    private Role role;
+
+
+}
