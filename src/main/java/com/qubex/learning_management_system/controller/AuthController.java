a2338cb3e49c4b16a03778316a4a92433208781a
Khairul Basar <khairulbashar24853@gmail.com>
2025-03-11 21:20:20 +0600

    Add Authentication controller

--- src/main/java/com/qubex/learning_management_system/controller/AuthController.java
+++ src/main/java/com/qubex/learning_management_system/controller/AuthController.java
@@ -1,6 +1,29 @@
 package com.qubex.learning_management_system.controller;
 
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth/")
+@RequiredArgsConstructor
 public class AuthController {
+
+    @GetMapping("/register")
+    public String getRegisterPage(Model model) {
+
+        return "auth/register";
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage(){
+
+        return "auth/login";
+
+    }
 
 
 }
