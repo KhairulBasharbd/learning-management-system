56e4b8a32b30433cc4944182754b65914ab760e8
Khairul Basar <khairulbashar24853@gmail.com>
2025-03-23 00:38:01 +0600

    Add functionality to store different role's registration data in different DB table

--- src/main/java/com/qubex/learn_now/service/UserService.java
+++ src/main/java/com/qubex/learn_now/service/UserService.java
@@ -1,14 +1,16 @@
 package com.qubex.learn_now.service;
 
-import com.fasterxml.jackson.core.Base64Variant;
 import com.qubex.learn_now.dto.request.UserRegistrationRequestDTO;
 import com.qubex.learn_now.exception.custom.AlreadyExistsException;
 import com.qubex.learn_now.mapper.UserRegistrationMapper;
+import com.qubex.learn_now.model.Instructor;
+import com.qubex.learn_now.model.Student;
 import com.qubex.learn_now.model.User;
+import com.qubex.learn_now.repository.InstructorRepository;
+import com.qubex.learn_now.repository.StudentRepository;
 import com.qubex.learn_now.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.config.authentication.PasswordEncoderParser;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -20,8 +22,10 @@ import java.util.HashSet;
 @RequiredArgsConstructor
 public class UserService {
 
+
     private final UserRepository userRepository;
-    private final UserRegistrationMapper userRegistrationMapper;
+    private final StudentRepository studentRepository;
+    private final InstructorRepository instructorRepository;
     private final PasswordEncoder passwordEncoder;
 
 
@@ -33,13 +37,36 @@ public class UserService {
 
         dto.setPassword(passwordEncoder.encode(dto.getPassword()));
 
-        User user = userRegistrationMapper.toEntity(dto);
-        user.setRoles(new HashSet<>(Collections.singletonList(dto.getRole())) );
+        log.info("User role :{}",dto.getRole().toString());
 
-        log.info("User role :{}",user.getRoles().toString());
+        if("role_student".equalsIgnoreCase(dto.getRole().toString())){
 
-        return userRepository.save(user);
+            Student student = new Student();
+
+            student.setFullName( dto.getFullName() );
+            student.setEmail( dto.getEmail() );
+            student.setPassword( dto.getPassword() );
+            student.setRoles(new HashSet<>(Collections.singletonList(dto.getRole())) );
+
+            return studentRepository.save(student);
+        }
+        else if("role_instructor".equalsIgnoreCase(dto.getRole().toString())){
+
+            Instructor instructor = new Instructor();
+
+            instructor.setFullName( dto.getFullName() );
+            instructor.setEmail( dto.getEmail() );
+            instructor.setPassword( dto.getPassword() );
+            instructor.setRoles(new HashSet<>(Collections.singletonList(dto.getRole())) );
+
+            return instructorRepository.save(instructor);
+        }
+        else  {
+            throw new IllegalArgumentException("Invalid role");
+        }
     }
+
+
 
 }
 
