21f00cba406fddc65cf3a0fa2a1bad62f2cce2b6
Khairul Basar <khairulbashar24853@gmail.com>
2025-03-23 00:36:08 +0600

    Make Unused UserRegistrationMapper.java

--- src/main/java/com/qubex/learn_now/mapper/UserRegistrationMapper.java
+++ src/main/java/com/qubex/learn_now/mapper/UserRegistrationMapper.java
@@ -2,11 +2,16 @@ package com.qubex.learn_now.mapper;
 
 
 import com.qubex.learn_now.dto.request.UserRegistrationRequestDTO;
+import com.qubex.learn_now.model.Instructor;
+import com.qubex.learn_now.model.Student;
 import com.qubex.learn_now.model.User;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface UserRegistrationMapper {
 
-    User toEntity(UserRegistrationRequestDTO userRegistrationRequestDTO);
+//    User toEntity(UserRegistrationRequestDTO userRegistrationRequestDTO);
+//
+
+
 }
