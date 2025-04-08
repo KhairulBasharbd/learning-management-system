cda8cccd671f6bce5b20c85d6344c880824817ef
Khairul Basar <khairulbashar24853@gmail.com>
2025-04-08 10:47:12 +0600

    Add Section.java Entity

--- src/main/java/com/qubex/learn_now/model/Section.java
+++ src/main/java/com/qubex/learn_now/model/Section.java
@@ -1,4 +1,54 @@
 package com.qubex.learn_now.model;
 
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "sections")
 public class Section {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @NotBlank(message = "Section title is required")
+    private String title;
+
+    @Lob
+    private String description;
+
+
+    @Column(name = "section_order")
+    private int order;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    @OneToMany(mappedBy = "section", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OrderBy("order ASC")
+    private List<Lecture> lectures = new ArrayList<>();
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
 }
