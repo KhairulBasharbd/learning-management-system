4ff08fe2eb867142b5c6976d31e1726b85a925b0
Khairul Basar <khairulbashar24853@gmail.com>
2025-04-08 10:44:22 +0600

    Add LectureCreationRequestDTO.java

--- src/main/java/com/qubex/learn_now/dto/request/LectureCreationRequestDTO.java
+++ src/main/java/com/qubex/learn_now/dto/request/LectureCreationRequestDTO.java
@@ -1,4 +1,18 @@
 package com.qubex.learn_now.dto.request;
 
+import com.qubex.learn_now.enums.LectureType;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
 public class LectureCreationRequestDTO {
+
+    private UUID id;
+    private String title;
+    private String description;
+    private int order;
+    private LectureType type;
+    private String content;
+    private UUID sectionId;
 }
