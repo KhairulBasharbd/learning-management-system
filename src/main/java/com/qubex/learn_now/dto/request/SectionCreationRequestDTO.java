b22f5a7b9e5db3d2f067969582bf540196f0e321
Khairul Basar <khairulbashar24853@gmail.com>
2025-04-08 10:47:32 +0600

    Add SectionCreationRequestDTO.java

--- src/main/java/com/qubex/learn_now/dto/request/SectionCreationRequestDTO.java
+++ src/main/java/com/qubex/learn_now/dto/request/SectionCreationRequestDTO.java
@@ -1,4 +1,16 @@
 package com.qubex.learn_now.dto.request;
 
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
 public class SectionCreationRequestDTO {
+
+    private UUID id;
+    private String title;
+    private String description;
+    private int order;
+    private UUID courseId;
+
 }
