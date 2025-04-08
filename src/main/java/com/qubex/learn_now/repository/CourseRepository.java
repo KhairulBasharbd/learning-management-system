0d2725cabcba58973f70b572268f83f59ebdb393
Khairul Basar <khairulbashar24853@gmail.com>
2025-04-08 10:37:39 +0600

    Add query in CourseRepository.java

--- src/main/java/com/qubex/learn_now/repository/CourseRepository.java
+++ src/main/java/com/qubex/learn_now/repository/CourseRepository.java
@@ -1,12 +1,20 @@
 package com.qubex.learn_now.repository;
 
 import com.qubex.learn_now.model.Course;
+import com.qubex.learn_now.model.Instructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.rmi.server.UID;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CourseRepository extends JpaRepository<Course, UUID> {
+
+
+    List<Course> findByInstructor(Instructor instructor);
+
+    Optional<Course> findById(UUID courseId);
 
 
 }
