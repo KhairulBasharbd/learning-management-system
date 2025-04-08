a71c382b90bf4d609223755ba932607c082eafed
Khairul Basar <khairulbashar24853@gmail.com>
2025-04-08 10:43:34 +0600

    Add Lecture.java Entity

--- src/main/java/com/qubex/learn_now/model/Lecture.java
+++ src/main/java/com/qubex/learn_now/model/Lecture.java
@@ -1,4 +1,54 @@
 package com.qubex.learn_now.model;
 
+
+import com.qubex.learn_now.enums.LectureType;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "lectures")
 public class Lecture {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @NotBlank(message = "Lecture title is required")
+    private String title;
+
+    private String description;
+
+    @Column(name = "lecture_order")
+    private int order;
+
+    @Enumerated(EnumType.STRING)
+    private LectureType type;
+
+    @Lob
+    private String content; // URL or text content
+
+    @ManyToOne
+    @JoinColumn(name = "section_id")
+    private Section section;
+
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
 }
