d01d7c8370869633c8425bf7412de5faf04ba54b
Khairul Basar <khairulbashar24853@gmail.com>
2025-04-08 10:35:18 +0600

    Add CourseCategory.java Entity

--- src/main/java/com/qubex/learn_now/model/CourseCategory.java
+++ src/main/java/com/qubex/learn_now/model/CourseCategory.java
@@ -1,6 +1,32 @@
 package com.qubex.learn_now.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "coursecategories")
+@Data
 public class CourseCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(unique = true)
+    private String name;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
 
 
 }
