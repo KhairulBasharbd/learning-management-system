1d98c2375b5aed07ccf583b95279fb9ea585738e
Khairul Basar <khairulbashar24853@gmail.com>
2025-04-08 10:39:19 +0600

    Change Instructor.java Entity

--- src/main/java/com/qubex/learn_now/model/Instructor.java
+++ src/main/java/com/qubex/learn_now/model/Instructor.java
@@ -1,10 +1,15 @@
 package com.qubex.learn_now.model;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name="instructors")
@@ -15,5 +20,13 @@ public class Instructor extends User {
     private String bio;
     private String expertise;
     private float averageRating;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
 
 }
