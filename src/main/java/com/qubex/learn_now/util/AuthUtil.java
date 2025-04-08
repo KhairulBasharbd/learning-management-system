229d33096c22ab3c8cd9e712ddf72e6a7dda8150
Khairul Basar <khairulbashar24853@gmail.com>
2025-04-08 10:32:51 +0600

    Add getAuthenticatedUsername function in AuthUtil.java

--- src/main/java/com/qubex/learn_now/util/AuthUtil.java
+++ src/main/java/com/qubex/learn_now/util/AuthUtil.java
@@ -1,6 +1,27 @@
 package com.qubex.learn_now.util;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
 public class AuthUtil {
 
+
+
+    private AuthUtil() {
+        throw new UnsupportedOperationException("Utility class - instantiation not allowed");
+    }
+
+    public static String getAuthenticatedUsername() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            Object principal = authentication.getPrincipal();
+            if (principal instanceof UserDetails userDetails) {
+                return userDetails.getUsername();
+            }
+            return principal.toString();
+        }
+        return null;
+    }
 
 }
