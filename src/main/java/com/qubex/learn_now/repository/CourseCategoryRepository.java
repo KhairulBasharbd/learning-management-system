f88abdf42a32dbc8e9a6edaa56b1850fa96bfa64
Khairul Basar <khairulbashar24853@gmail.com>
2025-04-12 20:20:33 +0200

    Change CourseCategoryRepository.java

--- src/main/java/com/qubex/learn_now/repository/CourseCategoryRepository.java
+++ src/main/java/com/qubex/learn_now/repository/CourseCategoryRepository.java
@@ -3,9 +3,11 @@ package com.qubex.learn_now.repository;
 import com.qubex.learn_now.model.CourseCategory;
 import com.qubex.learn_now.model.Instructor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface CourseCategoryRepository extends JpaRepository<CourseCategory, UUID> {
 
 
